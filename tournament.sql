ba99f026d727de9918560f4dc66f9d45f533e758
trinicoder <qpcc_lab@mh-qpcc-ws-021.mh.local>
2017-03-28 08:48:32 -0400

    loser ID

--- tournament.sql
+++ tournament.sql
@@ -10,10 +10,10 @@
 
 CREATE TABLE tournament ( playerName TEXT, id SERIAL );
 
-CREATE TABLE matches ( pID1 integer,pID2 integer, id SERIAL );  
-CREATE TABLE wins ( winner_ID integer,Loser_ID integer,match_ID integer, id SERIAL );            
+CREATE TABLE matches ( pID1 integer,pID2 integer, id SERIAL );
+CREATE TABLE wins ( winner_ID integer,Loser_ID integer,match_ID integer, id SERIAL );
 
-DROP TABLE [ IF EXISTS ] name [, ...] [ CASCADE | RESTRICT ];  
+DROP TABLE [ IF EXISTS ] name [, ...] [ CASCADE | RESTRICT ];
 DELETE FROM  tournament WHERE id = 2;
 DELETE FROM tournament;
 INSERT INTO matches VALUES (3,4 );
@@ -32,18 +32,25 @@ select * from tournament;
 SELECT id FROM matches WHERE pID1 = 1 and pID2= 2 or pID2 = 1 and pID1= 2;
 
 select id, playerName, num_wins
-from (SELECT winner_id, count(match_id) as num_wins 
-                    FROM wins 
-                    group by winner_id 
+from (SELECT winner_id, count(match_id) as num_wins
+                    FROM wins
+                    group by winner_id
                     order by num_wins)as R1  right join tournament on R1.winner_id= tournament.id;
 /* with loses and order by wins*/
 select id, playerName, num_wins
-from (SELECT winner_id, count(match_id) as num_wins 
-                    FROM wins 
-                    group by winner_id 
+from (SELECT winner_id, count(match_id) as num_wins
+                    FROM wins
+                    group by winner_id
                     order by num_wins)as R1  right join tournament on R1.winner_id= tournament.id order by num_wins;
 
-SELECT winner_id,tournament.playerName, count(match_id) as num_wins 
-                    FROM wins join tournament on winner_ID= tournament.id
-                    group by winner_id,tournament.playerName 
-                    order by num_wins;
+SELECT tournament.id,  tournament.name, count(match_id) as num_loses
+FROM wins left join tournament
+WHERE wins.Loser_ID = tournament.id
+group by tournament.id
+order by num_loses
+
+SELECT tournament.id,  tournament.name, count(match_id) as num_loses
+FROM wins left join tournament
+WHERE wins.Loser_ID = tournament.id
+group by tournament.id
+order by num_loses
